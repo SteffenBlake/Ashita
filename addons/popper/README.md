025c35df5d0c48a8fffa06505513000160d687d0
Steffen Cole Blake <steffen.cole.blake@gmail.com>
2020-09-08 20:39:07 -0600

    Popper v1.1.0

--- addons/popper/README.md
+++ addons/popper/README.md
@@ -2,14 +2,34 @@
 
 Simple addon to simplify pop items for ???s or any other NPC in the game.
 
+# Dependencies
+
+Requires the loading and usage of Lolwutt's Bellhop plugin
+https://docs.ashitaxi.com/plugins/bellhop/
+
+## /pop "Item-Name" # "Item Name" #
+
+Registers "Item-Name" as the pop for the currently targeted NPC, with associated numbers of trade values, and simultaneously "pops" it (trades that item)
+
+Example: `/pop "Snow God Core" 1 "Sisyphus Fragment" 1 "H.Q. Marid Hide" 1
+
 ## /pop "Item-Name"
 
-Registers "Item-Name" as the pop for the currently targeted NPC, and simultaneously "pops" it (trades that item)
+Shorthand for single item trades, synonymous with `/pop "Item-Name" 1`
 
 ## /pop
 
 Trades the registered pop item for an NPC.
 
+## /pop [save|load]
+
+Saves/Loads the current configuration to/from the settings file (Useful for multiboxing)
+
+## /pop autosave
+
+Toggles Autosave functionality on/off. When enabled, popper will automatically save newly registered pops or config changes, useful for multiboxing
+
+Disabled by default
 
 ## Examples
 
@@ -20,5 +40,7 @@ Save abyssea pop items for their respective "???"s
 Save "Silver Voucher" for Greyson
 
 Save "Copper Voucher" for your RoE NPCs.
+
+Register your millioncorns for Melyon
 
 Etc etc
--- addons/popper/popper.lua
+++ addons/popper/popper.lua
@@ -25,7 +25,7 @@
 
 _addon.author   = 'Soralin';
 _addon.name     = 'Popper';
-_addon.version  = '1.0.0';
+_addon.version  = '1.1.0';
 
 require 'common'
 require 'ffxi.targets'
@@ -33,16 +33,48 @@ require 'ffxi.targets'
 ----------------------------------------------------------------------------------------------------
 -- Configurations
 ----------------------------------------------------------------------------------------------------
-local default_config = {};
+local default_config = {
+	AutoSave = false;
+};
 local popper_config = default_config;
+
+local DebugMode = false;
+
+function ToggleDebugMode()
+	DebugMode = not DebugMode;
+	if (DebugMode) then
+		PrintAddon("Debug mode enabled.");
+	else
+		PrintAddon("Debug mode disabled.");
+	end
+end
+
+function PrintAddon(text)
+	print("Popper: " .. text);
+end
+
+function PrintDebug(text)
+	if (DebugMode) then
+		print("Popper debug: " .. text);
+	end
+end
+
+function ToggleAutoSave()
+	popper_config.AutoSave = not popper_config.AutoSave;
+	if (popper_config.AutoSave) then
+		PrintAddon("Auto Save enabled.");
+		SaveSettings();
+	else
+		PrintAddon("Auto Save disabled.");
+	end
+end
 
 ----------------------------------------------------------------------------------------------------
 -- func: load
 -- desc: Event called when the addon is being loaded.
 ----------------------------------------------------------------------------------------------------
 ashita.register_event('load', function()
-    -- Attempt to load the configuration..
-    popper_config = ashita.settings.load_merged(_addon.path .. 'settings/settings.json', popper_config);
+	LoadSettings();
 end);
 
 ----------------------------------------------------------------------------------------------------
@@ -50,49 +82,151 @@ end);
 -- desc: Event called when the addon is being unloaded.
 ----------------------------------------------------------------------------------------------------
 ashita.register_event('unload', function()
-    -- Attempt to save the configuration..
-    ashita.settings.save(_addon.path .. 'settings/settings.json', popper_config);
+    SaveSettings();
 end);
+
+function LoadSettings()
+	popper_config = ashita.settings.load_merged(_addon.path .. 'settings/settings.json', popper_config);
+	PrintAddon("Settings loaded!");
+	
+	if (popper_config.AutoSave == nil) then
+		popper_config.AutoSave = false;
+	end
+
+	-- Check if the config needs to update from 1.0.0 format to 1.1.0
+	for k, v in pairs(popper_config) do
+		if (type(v) == "table") then
+			return;
+		end
+
+		if (type(v) == "string") then
+			popper_config[k] = { };
+			popper_config[k][v] = 1;
+		end
+	end
+
+	PrintAddon("Detected 1.0.0 settings file, patching up to 1.1.0 format.");
+	SaveSettings();
+	PrintAddon("1.1.0 settings patch complete!");
+end
+
+function SaveSettings()
+	local data = ashita.settings.JSON:encode_pretty(popper_config);
+    local f = io.open(_addon.path .. 'settings/settings.json', 'w');
+	f:write(data);
+    f:close();
+end
+
+function PrintHelp()
+	PrintAddon("/pop -- Trades registered items for a targeted npc");
+	PrintAddon("/pop \"Item Name\" # \"Item Name\" # ... etc -- Registers a trade for a targetted NPC, then performs the trade");
+	PrintAddon("/pop [save|load] -- Saves/Loads the current configuration, useful for multiboxing");
+	PrintAddon("/pop autosave -- Toggles autosave functionality. When enabled, will automatically save new registrations.");
+end
 
 ----------------------------------------------------------------------------------------------------
 -- func: command -- Code adapted from drawdistance.
 -- desc: Event called when a command was entered.
 ----------------------------------------------------------------------------------------------------
 ashita.register_event('command', function(command, ntype)
-
-	-- Get the arguments of the command..
 	local args = command:args();
 	
 	if (args[1] ~= '/pop') then
 		return false;
 	end
 
+	if (#args == 1) then
+		Execute();
+		return true;
+	end
+
+	if (#args == 2) then
+		if (args[2] == "debug") then
+			ToggleDebugMode();
+			return true;
+		elseif (args[2] == "autosave") then
+			ToggleAutoSave();
+			return true;
+		elseif (args[2] == "save") then
+			SaveSettings();
+			return true;
+		elseif (args[2] == "load") then
+			LoadSettings();
+			return true;
+		elseif (args[2] == "help") then
+			PrintHelp();
+			return true;
+		else
+			args = {args[1], args[2], 1};
+		end
+	end
+
+	-- Even number of args is invalid input
+	if (#args % 2 == 0) then
+		PrintAddon("Unrecognized input!");
+		PrintDebug("Arg Count: " .. #args);
+		PrintHelp();
+		return true;
+	end
+
+	RegisterPop(args);
+	Execute();
+    return true;
+end);
+
+-- Target identity consists of "<ZoneId>-<TargetIndex>"
+function GetTargetIdentity() 
+	local zoneId = AshitaCore:GetDataManager():GetParty():GetMemberZone(0);
+
 	local target = ashita.ffxi.targets.get_target('t');
     if (target == nil or target.Name == '' or target.TargetIndex == 0) then
-		print('No target found');
+		PrintAddon('No target found');
 		return true;
 	end
 
-	local zoneId = AshitaCore:GetDataManager():GetParty():GetMemberZone(0);
 	local targetId = target.TargetIndex;
-
 	local targetIdentity = zoneId .. '-' .. targetId;
 
-	if (args[2] ~= nil) then
-		popper_config[targetIdentity] = args[2];
-		print('Registered pop "'..args[2]..'" for target: '.. targetIdentity)
+	return targetIdentity;
+end
+
+function RegisterPop(args)
+	local targetIdentity = GetTargetIdentity();
+
+	-- Create empty storage object
+	popper_config[targetIdentity] = {};
+
+	-- Save each of the ("Item Name", #) arg pairs to the object
+	for i=2, #args, 2 do
+		popper_config[targetIdentity][args[i]] = args[i+1];
 	end
 
+	if (popper_config.AutoSave) then
+		SaveSettings();
+	end
+
+	PrintAddon("Pop registered! Now popping")
+	PrintDebug('Registered pops for target: '.. targetIdentity)
+end
+
+function Execute()
+	local command = '/trade ';
+	local targetIdentity = GetTargetIdentity();
+
 	if (popper_config[targetIdentity] == nil) then
-		print('No registered item for target: '.. targetIdentity);
-		print ('Use /pop \"<itemName>\" to register a pop');
+		PrintAddon('No registered item(s) for this target.');
+		PrintDebug("TargetIdentity: " .. targetIdentity);
+		PrintAddon('Use /pop \"<itemName>\" # \"<itemName>\" # ... etc, to register a pop');
 		return true;
 	end
 
-	local command = '/item "' .. popper_config[targetIdentity] .. '" <t>';
-
-	AshitaCore:GetChatManager():QueueCommand(command, 1);
-
-    return true;
+	-- Build the command out of the config
+	for item, count in pairs(popper_config[targetIdentity]) do
+		command = command .. '"' .. item .. '" ' .. count;
 	end
-);
+
+	command = command .. " <t>";
+
+	PrintDebug("Executing: ".. command);
+	AshitaCore:GetChatManager():QueueCommand(command, 1);
+end
